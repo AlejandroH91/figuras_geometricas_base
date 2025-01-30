47e7ca52470f103451f8059bf3085ef0b1b02a66
Abeagom <beas.gomez.alberto@iescamas.es>
2025-01-30 10:01:00 +0100

    Añadidos primeros métodos

---
+++ code/src/main/java/com/endes/figuras/Cuadrado.java
@@ -0,0 +1,32 @@
+package com.endes.figuras;
+
+public class Cuadrado extends FiguraGeometrica {
+	private Punto esquina;
+	private double lado;
+	
+	public Cuadrado(String nombre, String color, Punto esquina, double lado) {
+		super(nombre, color);
+		this.esquina = esquina;
+		this.lado=lado;
+		
+	}
+
+	@Override
+	public double area() {
+		return lado*lado;
+	}
+
+	@Override
+	public double perimetro() {
+		return lado*4;
+	}
+	
+	public void rotar(double angulo) {
+		
+	}
+	
+	public void trasladar (double dx, double dy) {
+		
+	}
+
+}
